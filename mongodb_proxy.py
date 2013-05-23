a4cc0674c372c6cf49749baef8247c38d0a3659d
Jonathan Kamens <jik@quantopian.com>
2013-05-22 22:21:18 -0400

    Let AutoReconnect exception bubble up to caller after 5 failures

--- mongodb_proxy.py
+++ mongodb_proxy.py
@@ -11,13 +11,15 @@ def safe_mongocall(call):
     """
 
     def _safe_mongocall(*args, **kwargs):
-        for i in xrange(5):
+        for i in xrange(4):
             try:
                 return call(*args, **kwargs)
             except pymongo.errors.AutoReconnect:
                 print 'AutoReconnecting, try', i
                 time.sleep(pow(2, i))
-        print 'Error: Failed operation!'
+        # Try one more time, but this time, if it fails, let the
+        # exception bubble up to the caller.
+        return call(*args, **kwargs)
     return _safe_mongocall
 
 
