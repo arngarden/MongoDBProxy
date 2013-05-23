9d3c77b5da20c64b16d20d121f438ed26735a970
Jonathan Kamens <jik@quantopian.com>
2013-05-23 17:13:18 -0400

    Fix message that gets printed on reconnect attempts

--- mongodb_proxy.py
+++ mongodb_proxy.py
@@ -21,7 +21,7 @@ def safe_mongocall(call):
             try:
                 return call(*args, **kwargs)
             except pymongo.errors.AutoReconnect:
-                print 'AutoReconnecting, try', i
+                print 'AutoReconnecting, try %d' % i
                 time.sleep(pow(2, i))
         # Try one more time, but this time, if it fails, let the
         # exception bubble up to the caller.
