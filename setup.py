92476ba34c4d51b5b3b2365ff37aeae9efe3c450
Scott Sanderson <ssanderson@quantopian.com>
2017-01-08 21:00:56 -0500

    BLD: Remove need for pyandoc at setup time.
    Rather than converting README.md to .rst, just use an .rst file, which
    GitHub understands perfectly well.

--- setup.py
+++ setup.py
@@ -16,24 +16,8 @@ Copyright 2013 Gustav Arngarden
 
 from setuptools import setup, find_packages
 
-LONG_DESCRIPTION = None
-README_MARKDOWN = None
-
-with open('README.md') as markdown_source:
-    README_MARKDOWN = markdown_source.read()
-
-try:
-    import pandoc
-    pandoc.core.PANDOC_PATH = 'pandoc'
-    # Converts the README.md file to ReST, since PyPI uses ReST for formatting,
-    # This allows to have one canonical README file, being the README.md
-    doc = pandoc.Document()
-    doc.markdown = README_MARKDOWN
-    LONG_DESCRIPTION = doc.rst
-except ImportError:
-    # If pandoc isn't installed, e.g. when downloading from pip,
-    # just use the regular README.
-    LONG_DESCRIPTION = README_MARKDOWN
+with open('README.rst') as readme_file:
+    LONG_DESCRIPTION = readme_file.read()
 
 setup(
     name='MongoDBProxy',
